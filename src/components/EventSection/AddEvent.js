a5e21b455c7dac8cbf00874ac1602e56d4b5e715
Manish <chaurasiya.mns@gmail.com>
2020-09-13 21:22:39 +0530

    create event functionality added

--- src/components/EventSection/AddEvent.js
+++ src/components/EventSection/AddEvent.js
@@ -1,20 +1,41 @@
 import React, { useEffect, useState } from "react";
 import "./AddEvent.css";
-
 import { Modal, Button, Form, Input, DatePicker, Select } from "antd";
 import "antd/dist/antd.css";
 import { eventTypeListAPi } from "../../services/event";
+import Axios from "axios";
 
 const { Option } = Select;
-// const { RangePicker } = DatePicker;
 
-// componnet
 const AddEvent = () => {
   const [visible, setVisible] = useState(false);
   const [typeList, setTypeList] = useState([]);
+  const [addEventData, setAddEventData] = useState({});
+  const createEventUrl = "https://ik-react-task.herokuapp.com/events/";
+
+  const token =
+    "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyNTksInVzZXJuYW1lIjoiS21hbmlzaEBnbWFpbC5jb20iLCJleHAiOjE2MDA1Mzk3MDAsImVtYWlsIjoiS21hbmlzaEBnbWFpbC5jb20ifQ.IQmnjkHVkHkBpsk8T2MH26uM9Z_YEmCvWIVW5VMH2GQ";
+
+  console.log(addEventData);
+
+  const createEvent = () => {
+    return Axios.post(
+      createEventUrl,
+      { addEventData },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    )
+      .then((response) => console.log(response))
+      .catch((err) => err);
+  };
 
   const onFinish = (values) => {
-    console.log(values);
+    values.start = values.start.format();
+    values.end = values.end.format();
+    setAddEventData(values);
   };
 
   const eventTypeList = async () => {
@@ -23,6 +44,10 @@ const AddEvent = () => {
       setTypeList(lists);
     }
   };
+
+  useEffect(() => {
+    createEvent();
+  }, []);
 
   useEffect(() => {
     eventTypeList();
@@ -37,37 +62,26 @@ const AddEvent = () => {
         title="Add New Event"
         centered
         visible={visible}
-        // onOk={() => setVisible(false)}
         onCancel={() => setVisible(false)}
         width="40%"
         footer={null}
       >
-        <Form
-          name="nest-messages"
-          onFinish={onFinish}
-          //   validateMessages={validateMessages}
-        >
+        <Form name="nest-messages" onFinish={onFinish}>
           <Form.Item
-            // name={["user", "name"]}
-            label="Name"
+            name="name"
+            label="Event Name"
             rules={[{ required: true }]}
             style={{
               textAlign: "left",
               display: "flex",
-              backgroundColor: "red",
             }}
           >
             <Input />
           </Form.Item>
           <Form.Item
-            name="select"
-            label="Select"
-            rules={[
-              {
-                required: true,
-                message: "Please select the type of event!",
-              },
-            ]}
+            name="event_type"
+            label="Event Type"
+            rules={[{ required: true }]}
           >
             <Select placeholder="Event type">
               {typeList &&
@@ -79,20 +93,20 @@ const AddEvent = () => {
                 ))}
             </Select>
           </Form.Item>
-          {/* <Form.Item name="range-picker" label="RangePicker">
-            <RangePicker />
-          </Form.Item> */}
-          <Form.Item>
-            <label>Start Date</label>
-            <input type="date" />
+          <Form.Item
+            name="start"
+            label="Start Date"
+            rules={[{ required: true }]}
+          >
+            <DatePicker />
+          </Form.Item>
+
+          <Form.Item name="end" label="End Date" rules={[{ required: true }]}>
+            <DatePicker />
           </Form.Item>
           <Form.Item>
-            <Form.Item>
-              <label>End Date</label>
-              <input type="date" />
-            </Form.Item>
             <Button type="primary" htmlType="submit">
-              Submit
+              Add
             </Button>
           </Form.Item>
         </Form>
--- src/services/event.js
+++ src/services/event.js
@@ -6,17 +6,14 @@ const token =
   "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyNTksInVzZXJuYW1lIjoiS21hbmlzaEBnbWFpbC5jb20iLCJleHAiOjE2MDA1Mzk3MDAsImVtYWlsIjoiS21hbmlzaEBnbWFpbC5jb20ifQ.IQmnjkHVkHkBpsk8T2MH26uM9Z_YEmCvWIVW5VMH2GQ";
 
 export const eventTypeListAPi = () => {
-  return (
-    Axios.get(BaseUrl, {
-      headers: {
-        Authorization: token,
-      },
+  return Axios.get(BaseUrl, {
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((result) => {
+      console.log("result", result.data);
+      return result.data;
     })
-      // .then((response) => response)
-      .then((result) => {
-        console.log("result", result.data);
-        return result.data;
-      })
-      .catch((err) => {})
-  );
+    .catch((err) => {});
 };
